9e9c2ff83ebed36fca9110ac04aa01850edcb2e6
Anasse Zougarh <zougarh.anasse@gmail.com>
2017-11-21 08:48:22 +0100

    Indentation code et correction

--- client.c
+++ client.c
@@ -35,7 +35,7 @@ typedef struct Channel {
 	Message *listMessage; // les msg du channel
 	char nom[30];
 	int id;
-	int nb_client; 
+	int nb_client;
 } Channel;
 
 typedef struct Requete { // struct a echanger avec client
@@ -45,14 +45,13 @@ typedef struct Requete { // struct a echanger avec client
 } Requete;
 
 //id des requetes
-// 1 : pseudo user -> création server 
+// 1 : pseudo user -> création server
 // 2 : creation channel
 // 3 : join channel
 // 4 : leave channel
 // 5 : get channels
 
 int ajouter_channel(int id_user, int socket) { // retourne id channel
-
 	Requete r;
 	int found = 0;
 	r.instruction = 2;
@@ -61,12 +60,12 @@ int ajouter_channel(int id_user, int socket) { // retourne id channel
 	scanf("%s", &r.text);
 
 	/* envoi du message vers le serveur */
-    if ((send(socket, &r, sizeof(r),0)) < 0) {
+  if ((send(socket, &r, sizeof(r),0)) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
-    }
-     
-    printf("ajout envoye au serveur. \n");
+  }
+
+  printf("ajout envoye au serveur. \n");
 
 	r.id = -1;
 	/* lecture de la reponse en provenance du serveur */
@@ -81,33 +80,35 @@ int ajouter_channel(int id_user, int socket) { // retourne id channel
 			}
 		}
 	}
+
 	return r.id;
 }
 
 int is_in_list_channel(Channel c, Channel* listChannel) {
 	Channel *aux;
 	aux = listChannel;
+
 	while (aux != NULL) {
 		if(aux->id == c.id) {
 			return 1;
-		}	
-		else{
-			aux = aux->suiv;
-		}	
+		}
+
+		aux = aux->suiv;
 	}
+
 	return 0;
 }
 
 void get_list_channel(Channel *listChannel, int socket) {
-	Channel c;	
+	Channel c;
 	Requete r;
 	r.instruction = 5;
-	
+
 	/* envoi du message vers le serveur */
-    if ((send(socket, &r, sizeof(r),0)) < 0) {
+  if ((send(socket, &r, sizeof(r),0)) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
-    }
+  }
 
 	while(recv(socket, &c, sizeof(c),0) > 0) {
 		if (is_in_list_channel(c,listChannel) == 0) {
@@ -125,10 +126,11 @@ void get_list_channel(Channel *listChannel, int socket) {
 void fin_connection(int socket) {
 	Requete r;
 	r.instruction = 7;
+
 	if ((send(socket, &r, sizeof(r),0)) < 0) { // message pour finir la connection avec le server
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
-    }
+  }
 }
 
 const char* creation_user(int socket, int *id_user) {
@@ -138,89 +140,90 @@ const char* creation_user(int socket, int *id_user) {
 	r.instruction = 1;
 	r.id = -1;
 	scanf("%s", &r.text);
-	
+
 	char pseudo[30];
-	strcpy(pseudo, r.text); 
-      
-    /* envoi du message vers le serveur */
-    if ((send(socket, &r, sizeof(r),0)) < 0) {
+	strcpy(pseudo, r.text);
+
+  /* envoi du message vers le serveur */
+  if ((send(socket, &r, sizeof(r),0)) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
-    }
-     
-    printf("pseudo envoye au serveur. \n");
-                
-    /* lecture de la reponse en provenance du serveur */
+  }
+
+  printf("pseudo envoye au serveur. \n");
+
+  /* lecture de la reponse en provenance du serveur */
 	while (r.id == -1){
 		if ((recv(socket, &r, sizeof(r),0)) > 0) {
 			*id_user = r.id;
 		}
 	}
+
 	return pseudo;
 }
 
 int main(int argc, char **argv) {
-  
-    int 	socket_descriptor, 	/* descripteur de socket */
-		longueur; 		/* longueur d'un buffer utilisé */
-    sockaddr_in adresse_locale; 	/* adresse de socket local */
-    hostent *	ptr_host; 		/* info sur une machine hote */
-    servent *	ptr_service; 		/* info sur service */
-    char 	buffer[256];
-    char *	prog; 			/* nom du programme */
-    char *	host; 			/* nom de la machine distante */
-    char *	mesg; 			/* message envoyé */
+
+	int 	socket_descriptor, 	/* descripteur de socket */
+	longueur; 		/* longueur d'un buffer utilisé */
+	sockaddr_in adresse_locale; 	/* adresse de socket local */
+	hostent *	ptr_host; 		/* info sur une machine hote */
+	servent *	ptr_service; 		/* info sur service */
+	char 	buffer[256];
+	char *	prog; 			/* nom du programme */
+	char *	host; 			/* nom de la machine distante */
+	char *	mesg; 			/* message envoyé */
 	const char * pseudo; // pseudo de l'utilisateur
 
 	Message *listMessage = NULL; // messages du channel rejoind
 	Channel *listChannel = NULL; // liste des channels
-	int id_user; //id de l'utilisateur 
-     
-    if (argc != 2) {
-	perror("usage : client <adresse-serveur>");
-	exit(1);
-    }
-   
-    prog = argv[0];
-    host = argv[1];
-    
-    printf("nom de l'executable : %s \n", prog);
-    printf("adresse du serveur  : %s \n", host);
-    
-    if ((ptr_host = gethostbyname(host)) == NULL) {
-	perror("erreur : impossible de trouver le serveur a partir de son adresse.");
-	exit(1);
-    }
-    
-    /* copie caractere par caractere des infos de ptr_host vers adresse_locale */
-    bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
-    adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
-    
-    /*-----------------------------------------------------------*/
-    if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
-	perror("erreur : impossible de recuperer le numero de port du service desire.");
-	exit(1);
-    }
-    adresse_locale.sin_port = htons(ptr_service->s_port);
-    /*-----------------------------------------------------------*/
-    
-    printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port));
-    
-    /* creation de la socket */
-    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-	perror("erreur : impossible de creer la socket de connexion avec le serveur.");
-	exit(1);
-    }
-    
-    /* tentative de connexion au serveur dont les infos sont dans adresse_locale */
-    if ((connect(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+	int id_user; //id de l'utilisateur
+
+  if (argc != 2) {
+		perror("usage : client <adresse-serveur>");
+		exit(1);
+  }
+
+  prog = argv[0];
+  host = argv[1];
+
+  printf("nom de l'executable : %s \n", prog);
+  printf("adresse du serveur  : %s \n", host);
+
+  if ((ptr_host = gethostbyname(host)) == NULL) {
+		perror("erreur : impossible de trouver le serveur a partir de son adresse.");
+		exit(1);
+  }
+
+  /* copie caractere par caractere des infos de ptr_host vers adresse_locale */
+  bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
+  adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
+
+  /*-----------------------------------------------------------*/
+  if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
+		perror("erreur : impossible de recuperer le numero de port du service desire.");
+		exit(1);
+  }
+  adresse_locale.sin_port = htons(ptr_service->s_port);
+  /*-----------------------------------------------------------*/
+
+  printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port));
+
+  /* creation de la socket */
+  if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("erreur : impossible de creer la socket de connexion avec le serveur.");
+		exit(1);
+  }
+
+  /* tentative de connexion au serveur dont les infos sont dans adresse_locale */
+  if ((connect(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
 		perror("erreur : impossible de se connecter au serveur.");
 		exit(1);
-    }
-    
-    printf("connexion etablie avec le serveur. \n");
-    
-    pseudo = creation_user(socket_descriptor, &id_user);
+  }
+
+  printf("connexion etablie avec le serveur. \n");
+
+  pseudo = creation_user(socket_descriptor, &id_user);
 	printf("id user : %d \n", id_user);
 
 	printf("Action ?\n");
@@ -234,7 +237,7 @@ int main(int argc, char **argv) {
 		case 0:
 			fin_connection(socket_descriptor);
 			break;
-		case 1: 
+		case 1:
 			if(ajouter_channel(id_user,socket_descriptor) == 1)
 			{
 				printf("ajouter suite\n");
@@ -242,13 +245,12 @@ int main(int argc, char **argv) {
 			break;
 		default: break;
 	}
-    
-    printf("\nfin de la reception.\n");
-    
-    close(socket_descriptor);
-    
-    printf("connexion avec le serveur fermee, fin du programme.\n");
-    
-    exit(0);
-    
+
+  printf("\nfin de la reception.\n");
+
+  close(socket_descriptor);
+
+  printf("connexion avec le serveur fermee, fin du programme.\n");
+
+  exit(0);
 }
--- server.c
+++ server.c
@@ -7,7 +7,7 @@ gcc IRC-C/server.c -lpthread -o server
 #include <linux/types.h> 	/* pour les sockets */
 #include <sys/socket.h>
 #include <netdb.h> 		/* pour hostent, servent */
-#include <string.h> 		/* pour bcopy, ... */  
+#include <string.h> 		/* pour bcopy, ... */
 #include <pthread.h>
 #define TAILLE_MAX_NOM 256
 
@@ -37,7 +37,7 @@ typedef struct Channel {
 	Message *listMessage; // les msg du channel
 	char nom[30];
 	int id;
-	int nb_client; 
+	int nb_client;
 } Channel;
 
 typedef struct Requete { // struct a echanger avec client
@@ -45,14 +45,15 @@ typedef struct Requete { // struct a echanger avec client
 	char text[256];
 	int id;
 } Requete;
-	
-	
+
+
 void nouveau_client(Client *list,int *nbclient, char pseudo[], int socket)
 {
 	Client *new = (Client*) malloc(sizeof(Client)); //crée new
 	*nbclient = (*nbclient) +1; //incrémente l'id
 	new->id = *nbclient; // assigne l'id
 	strcpy(new->pseudo, pseudo); // copie le pseudo
+
 	if(list == NULL) {
 		new->suiv = NULL;
 	}
@@ -68,14 +69,14 @@ void nouveau_client(Client *list,int *nbclient, char pseudo[], int socket)
 	if ((send(socket, &r, sizeof(r),0)) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
-    }
+  }
 	printf("ajout de l'user : %s id: %d \n", pseudo, new->id);
 }
 
 void supprimer_client(Client *list,int id_client)
 {
 	Client *aux;
-	Client *pre; 
+	Client *pre;
 	if(list != NULL) {
 		if(list->id == id_client) {
 			aux = list;
@@ -106,6 +107,7 @@ void creer_channel(Channel *list,int *nbchannel, char nom[], int socket)
 	Channel *new = (Channel*) malloc(sizeof(Channel)); //crée new
 	id_new = (*nbchannel) + 1; //incrémente l'id
 	*nbchannel = id_new; //assigne a la valeur de programme principal
+
 	new->id = id_new; // assigne l'id
 	new->nb_client = 0;
 	strcpy(new->nom, nom); // copie le nom
@@ -113,14 +115,13 @@ void creer_channel(Channel *list,int *nbchannel, char nom[], int socket)
 	list = new; // on fait pointer le début de la liste sur le nouveau
 	printf("ajout du channel %s ok\n", new->nom);
 
-
-	//envoie au client 
+	//envoie au client
 	Requete r;
 	r.id = new->id;
 	if ((send(socket, &r, sizeof(r),0)) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au client.");
 		exit(1);
-    }
+  }
 	printf("ajout du channel : %s id: %d \n", new->nom, new->id);
 }
 
@@ -128,8 +129,9 @@ int ajout_client_channel(Channel *channel, Client *list, int id_client) // retou
 {
 	int fait = 0;
 	Client *move = NULL;
-	Client *aux; 
+	Client *aux;
 	Client *pre;
+
 	if(list != NULL) {
 		if(list->id == id_client) {
 			move = list;//transfert
@@ -140,7 +142,7 @@ int ajout_client_channel(Channel *channel, Client *list, int id_client) // retou
 			aux = list->suiv;
 			while(aux != NULL){
 				if(aux->id == id_client){
-					move = aux;//transfert 
+					move = aux;//transfert
 					pre->suiv = aux->suiv; //enleve de la liste
 					break;
 				}
@@ -150,20 +152,23 @@ int ajout_client_channel(Channel *channel, Client *list, int id_client) // retou
 				}
 			}
 		}
+
 		if(move != NULL) {
 			fait = 1;
-			aux = (*channel).listClient;//prend la tete de la liste du channel
-			move->suiv = aux;//affecte la liste apres le client a ajouter
+			aux = (*channel).listClient; //prend la tete de la liste du channel
+			move->suiv = aux; //affecte la liste apres le client a ajouter
 			(*channel).listClient = move;
 		}
 	}
+
 	return fait;
 }
 
 void supprimer_channel(Channel *list,int id_channel)
 {
 	Channel *aux;
-	Channel *pre; 
+	Channel *pre;
+
 	if(list != NULL) {
 		if(list->id == id_channel) {
 			aux = list;
@@ -187,11 +192,11 @@ void supprimer_channel(Channel *list,int id_channel)
 	}
 }
 
-void supprimer_message(Message *list)//supprime le premier de la liste 
+void supprimer_message(Message *list) //supprime le premier de la liste
 {
 	Message *aux = list;
 	list = list->suiv;
-	free(aux); 
+	free(aux);
 }
 
 /*------------------------------------------------------*/
@@ -211,9 +216,10 @@ void *gestion_message (void * arg) {
 	int *nbclient = (int*)p3;
 	Channel *listChannel = (Channel*)p4;
 	int *nbchannel = (int*)p5;
-	int i = 1; 
-	while (i == 1) { 
-		if(recv(sock, &r, sizeof(r),0) > 0) { // assign la requete a 
+
+	int i = 1;
+	while (i == 1) {
+		if(recv(sock, &r, sizeof(r),0) > 0) { // assign la requete a
 			printf("Requete de type %d \n", r.instruction);
 
 			switch(r.instruction) {
@@ -223,7 +229,7 @@ void *gestion_message (void * arg) {
 				case 2:
 					creer_channel(listChannel, nbchannel, r.text, sock);
 					break;
-				case 7: //Id d'instruction pour fermer la connection 
+				case 7: //Id d'instruction pour fermer la connection
 					close(sock);
 					pthread_exit(NULL);
 					return;
@@ -237,98 +243,100 @@ void *gestion_message (void * arg) {
 
 /*------------------------------------------------------*/
 int main(int argc, char **argv) {
-  
-    int 		socket_descriptor, 		/* descripteur de socket */
-			nouv_socket_descriptor, 	/* [nouveau] descripteur de socket */
-			longueur_adresse_courante; 	/* longueur d'adresse courante d'un client */
-    sockaddr_in 	adresse_locale, 		/* structure d'adresse locale*/
-			adresse_client_courant; 	/* adresse client courant */
-    hostent*		ptr_hote; 			/* les infos recuperees sur la machine hote */
-    servent*		ptr_service; 			/* les infos recuperees sur le service de la machine */
-    char 		machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
-    
-    gethostname(machine,TAILLE_MAX_NOM);		/* recuperation du nom de la machine */
+
+	int socket_descriptor,		/* descripteur de socket */
+		nouv_socket_descriptor, 	/* [nouveau] descripteur de socket */
+		longueur_adresse_courante; 	/* longueur d'adresse courante d'un client */
+
+	sockaddr_in adresse_locale, 		/* structure d'adresse locale*/
+		adresse_client_courant; 	/* adresse client courant */
+
+	hostent* ptr_hote; 			/* les infos recuperees sur la machine hote */
+	servent* ptr_service; 			/* les infos recuperees sur le service de la machine */
+	char machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
+
+	gethostname(machine,TAILLE_MAX_NOM);		/* recuperation du nom de la machine */
 
 	Client *listClient = (Client*) malloc(sizeof(Client));
 	Channel *listChannel = (Channel*) malloc(sizeof(Channel));
+
 	int nb_messages = 0;
 	int nb_channels = 0;
 	int nb_clients = 0;
-    
-    /* recuperation de la structure d'adresse en utilisant le nom */
-    if ((ptr_hote = gethostbyname(machine)) == NULL) {
+
+  /* recuperation de la structure d'adresse en utilisant le nom */
+  if ((ptr_hote = gethostbyname(machine)) == NULL) {
 		perror("erreur : impossible de trouver le serveur a partir de son nom.");
 		exit(1);
-    }
-    
-    /* initialisation de la structure adresse_locale avec les infos recuperees */			
-    
-    /* copie de ptr_hote vers adresse_locale */
-    bcopy((char*)ptr_hote->h_addr, (char*)&adresse_locale.sin_addr, ptr_hote->h_length);
-    adresse_locale.sin_family		= ptr_hote->h_addrtype; 	/* ou AF_INET */
-    adresse_locale.sin_addr.s_addr	= INADDR_ANY; 			/* ou AF_INET */
+  }
 
-    /*-----------------------------------------------------------*/
-    /* SOLUTION 1 : utiliser un service existant, par ex. "irc" */
-    /* Utilisation de la solution 1 car application d'un service deja existant */
-    
-    if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
+  /* initialisation de la structure adresse_locale avec les infos recuperees */
+
+  /* copie de ptr_hote vers adresse_locale */
+  bcopy((char*)ptr_hote->h_addr, (char*)&adresse_locale.sin_addr, ptr_hote->h_length);
+  adresse_locale.sin_family		= ptr_hote->h_addrtype; 	/* ou AF_INET */
+  adresse_locale.sin_addr.s_addr	= INADDR_ANY; 			/* ou AF_INET */
+
+  /*-----------------------------------------------------------*/
+  /* SOLUTION 1 : utiliser un service existant, par ex. "irc" */
+  /* Utilisation de la solution 1 car application d'un service deja existant */
+
+  if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
 		perror("erreur : impossible de recuperer le numero de port du service desire.");
 		exit(1);
-    }
-    adresse_locale.sin_port = htons(ptr_service->s_port);
-    
-    printf("numero de port pour la connexion au serveur : %d \n", 
-		   ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
-    
-    /* creation de la socket */
-    if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  }
+
+  adresse_locale.sin_port = htons(ptr_service->s_port);
+  printf("numero de port pour la connexion au serveur : %d \n",
+	   ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
+
+  /* creation de la socket */
+  if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("erreur : impossible de creer la socket de connexion avec le client.");
 		exit(1);
-    }
+  }
 
-    /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
-    if ((bind(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
+  /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
+  if ((bind(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
 		perror("erreur : impossible de lier la socket a l'adresse de connexion.");
 		exit(1);
-    }
-    
-    /* initialisation de la file d'ecoute */
-    listen(socket_descriptor,5);
+  }
 
-    /* attente des connexions et traitement des donnees recues */
-    for(;;) {
-    	
+  /* initialisation de la file d'ecoute */
+  listen(socket_descriptor,5);
+
+  /* attente des connexions et traitement des donnees recues */
+  for(;;) {
 		longueur_adresse_courante = sizeof(adresse_client_courant);
-		
+
 		/* adresse_client_courant sera renseigné par accept via les infos du connect */
-		if ((nouv_socket_descriptor = 
-			accept(socket_descriptor, 
-			       (sockaddr*)(&adresse_client_courant),
-			       &longueur_adresse_courante))
-			 < 0) {
+		if ((nouv_socket_descriptor =
+			accept(
+				socket_descriptor,
+			  (sockaddr*)(&adresse_client_courant),
+			  &longueur_adresse_courante)
+			)
+			 < 0)
+		{
 			perror("erreur : impossible d'accepter la connexion avec le client.");
 			exit(1);
 		}
-		
+
 		/* traitement du message */
 		printf("reception d'un message.\n");
-		
+
 		pthread_t thread;
 		void *ptr[5];
 		ptr[0] = &nouv_socket_descriptor;
 		ptr[1] = listClient;
 		ptr[2] = &nb_clients;
 		ptr[3] = listChannel;
-		ptr[4] = &nb_channels; 
+		ptr[4] = &nb_channels;
 
 		if (pthread_create(&thread, NULL, gestion_message, ptr) == -1){
 			perror("pthread_create");
 		}
 		//gestion_message(nouv_socket_descriptor, listClient, &nb_clients,listChannel, &nb_channels);
 		//close(nouv_socket_descriptor);
-		
-    }
-    
+  }
 }
-
